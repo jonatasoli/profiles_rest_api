e06e4893f9e90fb8ee460cdd62dcf704a2daa36f
Jonatas Oliveira <contato@jonatasoliveira.me>
2017-11-09 11:13:15 -0200

    Added profile listing and creating feature

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
+from . import models
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -98,3 +99,9 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles deleting an object."""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, creating and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
